43a247de82dae6b3953ba4ba974d8029409a3009
KadenR <kadenreynolds05@gmail.com>
2023-11-07 13:05:44 -0700

    Set Selected started

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import ContactList from './Components/ContactList'
+import SelectedContact from './Components/SelectedContact'
 import './App.css'
 
 function App() {
-
+  const [selectedContactId, setSelectedContactId] = useState(null)
   return (
     <>
-    <ContactList />
+    {!selectedContactId ? (
+    <ContactList setSelectedContactId={setSelectedContactId}/>
+    )
+    :
+    (
+    <div>There is a contact selected</div>
+    )
+  }
     </>
   )
 }
--- src/Components/ContactList.jsx
+++ src/Components/ContactList.jsx
@@ -1,7 +1,27 @@
 import {useState, useEffect} from "react"
-import ContactRow from "./ContactRow"
 
-export default function ContactList () {
+
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
+
+export default function ContactList ({setSelectedContactId}) {
+  const [contactArray, setContactArray] = useState([])
+
+  useEffect(() => {
+    async function fetchAllContacts () {
+      try {
+        let response = await fetch("https://jsonplaceholder.typicode.com/users")
+        let data = await response.json()
+        setContactArray(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchAllContacts()
+  })
 
   return( 
     <table cellPadding={25}>
@@ -16,7 +36,15 @@ export default function ContactList () {
           <td className="lable">Email:</td>
           <td className="lable">Phone:</td>
         </tr>
-        <ContactRow />
+        {contactArray.map((contact) => {
+          return( 
+          <tr key={contact.id} onClick={() => {setSelectedContactId(contact)}}>
+            <td>{contact.name}</td>
+            <td>{contact.email}</td>
+            <td>{contact.phone}</td>
+          </tr>
+          )
+        })}
       </tbody>
     </table>
   )
